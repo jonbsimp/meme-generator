66c7dc56a7ef3613f09d92b57df51950ffa617e1
Ubuntu <ubuntu@ip-172-31-21-12.us-west-2.compute.internal>
2019-09-05 00:03:39 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-function App() {
+
+class App extends React.Component {
+  constructor(props) {
+    super(props) 
+      this.state= {
+        memes: [],
+      };
+  }
+  componentDidMount() {
+   fetch("https://ronreiter-meme-generator.p.rapidapi.com/meme?font=Impact&font_size=50&meme=Condescending-Wonka&top=Top%20text&bottom=Bottom%20text", {
+      "method": "GET",
+      "headers": {
+          "x-rapidapi-host": "ronreiter-meme-generator.p.rapidapi.com",
+          "x-rapidapi-key": "968da4de8emshb9e1c3c7ed0d63ap102a01jsn97f0dcc9733b"
+          }
+      })
+      .then(response => {
+          console.log(response);
+          return response.body
+      })
+      .then(body => {
+        const reader = body.getReader()
+        return reader.read()
+      })
+      .then(stream => {
+        console.log(stream)
+      })
+      .catch(err => {
+          console.log(err);
+      });
+  }
+  
+  
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jumbotron fluid>
+        
+          <h1>Meme Generator</h1>
+          <p>Search for memes</p>
+          <Form>
+            <div className='form-inline'>
+                <Form.Control placeholder="Search" />
+                <Button>Search</Button>
+            </div>
+          </Form>
+      </Jumbotron>
+      <div>
+      <h2>
+      {this.state.memes}
+      </h2>
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title></Card.Title>
+            <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+            <Card.Text>
+            
+            </Card.Text>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
+      </div>
+      
     </div>
-  );
+   );
+  }
 }
+
 
 export default App;
